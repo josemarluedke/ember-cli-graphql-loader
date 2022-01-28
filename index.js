956d7136c670c0eaeb1f3549b26abd14136d5443
Josemar Luedke <josemarluedke@gmail.com>
2022-01-28 14:01:34 -0800

    Implement addon

--- index.js
+++ index.js
@@ -2,4 +2,20 @@
 
 module.exports = {
   name: require('./package').name,
+
+  setupPreprocessorRegistry(type, registry) {
+    if (type === 'parent') {
+      registry.add('js', {
+        name: require('./package').name,
+        ext: 'graphql',
+        toTree(tree) {
+          const GraphQLFilter = require('broccoli-graphql-filter');
+
+          return new GraphQLFilter(tree, {
+            keepExtension: true,
+          });
+        },
+      });
+    }
+  },
 };
